9255f9df4f8dbaa5854b411e0d1f40113de55285
CalleEklund <eklund.calle@gmail.com>
2022-12-05 22:33:03 +0100

    added basic homepage, background image, font, custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mainbg': "url('../images/bg-image.jpg')"
+      },
+      colors: {
+        orange: "#f99e1a",
+        gray: "#43484c",
+        darkblue: "#405275",
+        blue: "#218ffe",
+        black: "#000000"
+      }
+    },
+
   },
   plugins: [],
 };
